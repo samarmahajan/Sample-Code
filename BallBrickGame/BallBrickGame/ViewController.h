6aab836a1634b13f319f04fcf761a6d9a9ec001f
Samar Gupta <tech.samar@gmail.com>
2013-09-14 21:50:52 +0530

    Brick Logic implemented.. and score login and lives logic implemented.

--- BallBrickGame/BallBrickGame/ViewController.h
+++ BallBrickGame/BallBrickGame/ViewController.h
@@ -7,6 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/CADisplayLink.h>
+
+#define BRICKS_WIDTH 5
+#define BRICKS_HEIGHT 4
+
 
 @interface ViewController : UIViewController {
     UILabel *scoreLabel;
@@ -15,14 +20,32 @@
     UIImageView *ball;
     UIImageView *paddle;
     CGPoint ballMovement;
+    float touchOffset;
+    
+    int lives;
+    UILabel *livesLabel;
+    UILabel *messageLabel;
+    BOOL isPlaying;
+    CADisplayLink *theTimer;
+    
+    UIImageView *bricks[BRICKS_WIDTH][BRICKS_HEIGHT];
+    NSString *brickTypes[4];
 
 }
 
 @property (nonatomic,strong)IBOutlet UILabel *scoreLabel;
 @property (nonatomic,strong)IBOutlet UIImageView *ball;
 @property (nonatomic,strong)IBOutlet UIImageView *paddle;
+@property (nonatomic, retain) IBOutlet UILabel *livesLabel;
+@property (nonatomic, retain) IBOutlet UILabel *messageLabel;
 
--(void)initializeTimer;
--(void)accelerometerChanges;
--(void)animateBall:(NSTimer *)theTimer;
+
+- (void)initializeTimer;
+- (void)accelerometerChanges;
+- (void)animateBall:(NSTimer *)theTimer;
+- (void)startPlaying;
+- (void)pauseGame;
+
+
+- (void)initializeBricks;
 @end
--- BallBrickGame/BallBrickGame/ViewController.m
+++ BallBrickGame/BallBrickGame/ViewController.m
@@ -22,7 +22,8 @@
 @synthesize scoreLabel;
 @synthesize ball;
 @synthesize paddle;
-
+@synthesize livesLabel;
+@synthesize messageLabel;
 
 - (void)viewDidLoad
 {
@@ -30,9 +31,53 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     ballMovement = CGPointMake(4.0, 4.0);
-    [self initializeTimer];
+    [self startPlaying];
     [self accelerometerChanges];
+    [self initializeBricks];
 }
+
+- (void)initializeBricks
+{
+    brickTypes[0] = @"bricktype1.png";
+    brickTypes[1] = @"bricktype2.png";
+    brickTypes[2] = @"bricktype3.png";
+    brickTypes[3] = @"bricktype4.png";
+    int count = 0;
+    for (int y = 0; y < BRICKS_HEIGHT; y++)
+    {
+        for (int x = 0; x < BRICKS_WIDTH; x++)
+        {
+            UIImage *image = [UIImage imageNamed:
+                              brickTypes[count++ % 4]];
+            bricks[x][y] = [[UIImageView alloc] initWithImage:image];
+            CGRect newFrame = bricks[x][y].frame;
+            newFrame.origin = CGPointMake(x * 64, (y * 40) + 100);
+            bricks[x][y].frame = newFrame;
+            [self.view addSubview:bricks[x][y]];
+        } }
+}
+
+- (void)startPlaying {
+    if (!lives) {
+        lives = 3;
+        score = 0; }
+    scoreLabel.text = [NSString stringWithFormat:@"%05d", score];
+    livesLabel.text = [NSString stringWithFormat:@"%d", lives];
+    ball.center = CGPointMake(159, 239);
+    ballMovement = CGPointMake(4,4);
+    // choose whether the ball moves left to right or right to left
+    if (arc4random() % 100 < 50)
+        ballMovement.x = -ballMovement.x;
+    
+    messageLabel.hidden = YES;
+    isPlaying = YES;
+    [self initializeTimer];
+}
+- (void)pauseGame {
+    [theTimer invalidate];
+    theTimer = nil;
+}
+
 -(void)accelerometerChanges
 {
     motionManager = [[CMMotionManager alloc] init];
@@ -58,34 +103,160 @@
 }
 
 
--(void)initializeTimer
+- (void)initializeTimer
 {
-    float theInterval = 1.0/30.0;
-    [NSTimer scheduledTimerWithTimeInterval:theInterval target:self selector:@selector(animateBall:) userInfo:nil repeats:YES];
-
-
+    if (theTimer == nil)
+    {
+        theTimer = [CADisplayLink displayLinkWithTarget:self
+                                               selector:@selector(animateBall:)];
+        theTimer.frameInterval = 2;
+        [theTimer addToRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
+    }
 }
 
--(void)animateBall:(NSTimer *)theTimer
+-(void)animateBall:(NSTimer *)theTimer1
 {
     ball.center = CGPointMake(ball.center.x + ballMovement.x, ball.center.y + ballMovement.y);
+    
+    
+    BOOL paddleCollision = ball.center.y >= paddle.center.y - 16 &&
+    ball.center.y <= paddle.center.y + 16 &&
+    ball.center.x > paddle.center.x - 32 &&
+    ball.center.x < paddle.center.x + 32;
+    
+    if (paddleCollision)
+    {
+        ballMovement.y = -ballMovement.y;
+        if (ball.center.y >= paddle.center.y - 16 && ballMovement.y < 0) {
+            ball.center = CGPointMake(ball.center.x, paddle.center.y - 16);
+        } else if (ball.center.y <= paddle.center.y + 16 && ballMovement.y > 0) {
+            ball.center = CGPointMake(ball.center.x, paddle.center.y + 16);
+        } else if (ball.center.x >= paddle.center.x - 32 && ballMovement.x < 0) {
+            ball.center = CGPointMake(paddle.center.x - 32, ball.center.y);
+        } else if (ball.center.x <= paddle.center.x + 32 && ballMovement.x > 0) {
+            ball.center = CGPointMake(paddle.center.x + 32, ball.center.y);
+        }
+    }
+    
+    BOOL there_are_solid_bricks = NO;
+    for (int y = 0; y < BRICKS_HEIGHT; y++)
+    {
+        for (int x = 0; x < BRICKS_WIDTH; x++)
+        {
+            if (1.0 == bricks[x][y].alpha)
+            {
+                there_are_solid_bricks = YES;
+                if ( CGRectIntersectsRect(ball.frame, bricks[x][y].frame) )
+                {
+                    [self processCollision:bricks[x][y]];
+                }
+            } else {
+                if (bricks[x][y].alpha > 0)
+                    bricks[x][y].alpha -= 0.1;
+            }
+        }
+    }
+    
+    if (!there_are_solid_bricks) {
+        [theTimer invalidate];
+        isPlaying = NO;
+        lives = 0;
+        messageLabel.text = @"You Win!";
+        messageLabel.hidden = NO;
+    }
+    
     
     if (ball.center.x > 300 || ball.center.x < 20)
     {
         ballMovement.x = -ballMovement.x;
     }
-    if (ball.center.y > 440 || ball.center.y < 40)
+    if (ball.center.y > 444 || ball.center.y < 40)
     {
         ballMovement.y = -ballMovement.y;
     }
 
+    
+    if (ball.center.y < 32)
+        ballMovement.y = -ballMovement.y;
+    if (ball.center.y > 444)
+    {
+        [self pauseGame];
+        isPlaying = NO;
+        lives--;
+        livesLabel.text = [NSString stringWithFormat:@"%d", lives];
+        if (!lives) {
+            messageLabel.text = @"Game Over";
+        } else {
+            messageLabel.text = @"Ball Out of Bounds";
+        }
+        messageLabel.hidden = NO;
+    }
 }
 
+- (void)processCollision:(UIImageView *)brick
+{
+    score += 10;
+    scoreLabel.text = [NSString stringWithFormat:@"%d", score];
+    if (ballMovement.x > 0 && brick.frame.origin.x - ball.center.x <= 4)
+        ballMovement.x = -ballMovement.x;
+    else if (ballMovement.x < 0 && ball.center.x - (brick.frame.origin.x +
+                                                    brick.frame.size.width) <= 4)
+        ballMovement.x = -ballMovement.x;
+    if (ballMovement.y > 0 && brick.frame.origin.y - ball.center.y <= 4)
+        ballMovement.y = -ballMovement.y;
+    else if (ballMovement.y < 0 && ball.center.y - (brick.frame.origin.y +
+                                                    brick.frame.size.height) <= 4)
+        ballMovement.y = -ballMovement.y;
+    brick.alpha -= 0.1;
+}
+
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+    if (isPlaying)
+    {
+        UITouch *touch = [[event allTouches] anyObject];
+        touchOffset = paddle.center.x - [touch locationInView:touch.view].x;
+    }
+    else
+    {
+        [self startPlaying];
+    }
+    
+
+}
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (isPlaying)
+    {
+        UITouch *touch = [[event allTouches] anyObject];
+        float distanceMoved = ([touch locationInView:touch.view].x + touchOffset) - paddle.center.x;
+        float newX = paddle.center.x + distanceMoved;
+        
+        if (newX > 30 && newX < 290)
+        {
+            paddle.center = CGPointMake(newX, paddle.center.y);
+        }
+        if (newX > 290)
+        {
+            paddle.center = CGPointMake(290, paddle.center.y);
+        }
+        if (newX < 30)
+        {
+            paddle.center = CGPointMake(30, paddle.center.y);
+        }
+
+    }
+    
+    
+}
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
